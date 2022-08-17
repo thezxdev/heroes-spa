66bb3ae8cd556bfcf4b04dfd1008413b38825191
thezxdev <thezxdev@gmail.com>
2022-08-17 03:09:56 -0700

    Animaciones con animate

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -28,7 +28,7 @@ export const HeroPage = () => {
         <img
           src={ `/assets/heroes/${ id }.jpg` }
           alt={ hero.superhero }
-          className="img-thumbnail"
+          className="img-thumbnail animate__animated animate__fadeInLeft"
         />
       </div>
 
