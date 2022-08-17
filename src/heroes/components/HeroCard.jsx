84589246d2a456c8a27bc29b917c09de5d050f24
thezxdev <thezxdev@gmail.com>
2022-08-16 19:33:36 -0700

    Componente para mostrardiferentes personajes

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom";
+
+const CharactesByHero = ({ alter_ego, characters }) => {
+
+  if( alter_ego === characters ) return (<></>);
+
+  return <p>{ characters }</p>
+
+}
+
 export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance, characters }) => {
   
   const heroImageUrl = `/assets/heroes/${ id }.jpg`;
   
+  // const charactesByHero = (<p>{ characters }</p>)
+
   return (
     <div className="col">
       <div className="card">
@@ -15,7 +27,22 @@ export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance
             <div className="card-body">
               <h5 className="card-title">{ superhero}</h5>
               <p className="card-text">{ alter_ego }</p>
-              <p>{ characters }</p>
+              
+              {/*
+                {
+                  ( alter_ego !== characters && (<p>{ characters }</p>) )
+                }
+              */}
+
+              <CharactesByHero alter_ego={ alter_ego } characters={ characters } />
+
+              <p className="card-text">
+                <small className="text-muted">{ first_appearance }</small>
+              </p>
+
+              <Link to={`/hero/${ id }`}>
+                Más...
+              </Link>
             </div>
           </div>
 
