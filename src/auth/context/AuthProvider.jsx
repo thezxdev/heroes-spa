f1a53a9320bce610613b622502d8e0bf020810df
thezxdev <thezxdev@gmail.com>
2022-08-17 22:29:32 -0700

    Mantener y recuperar el la sesion del localstorage

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -2,23 +2,36 @@ import { useReducer } from 'react';
 import { AuthContext, authReducer } from './';
 import { types } from '../types/types';
 
-const initialState = {
-  logged: false,
+// const initialState = {
+//   logged: false,
+// }
+
+const init = () => {
+  const user = JSON.parse( localStorage.getItem('user') );
+
+  return {
+    logged: !!user,
+    user
+  }
 }
 
 export const AuthProvider = ({ children }) => {
 
-  const [ authState, dispatch ] = useReducer( authReducer, initialState );
+  const [ authState, dispatch ] = useReducer( authReducer, {}, init );
 
   const login = ( name = '' ) => {
     
+    const user = {
+      id: 'ABC',
+      name
+    }
+
     const action = {
       type: types.login,
-      payload: {
-        id: 'ABC',
-        name
-      }
+      payload: user
     }
+
+    localStorage.setItem('user', JSON.stringify( user ) );
     
     dispatch( action );
   }
